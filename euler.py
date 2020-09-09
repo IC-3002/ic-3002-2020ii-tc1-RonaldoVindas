70b1f64e98bd0b7925e81e504f0808d9f45c1c83
RonaldoVindas <rony1211@gmail.com>
2020-09-09 15:36:58 -0600

    Primera versión prueba Euler.

--- euler.py
+++ euler.py
@@ -1,8 +1,14 @@
 def e_cuadratica(n):
     # Implemente esta función
-    return 0
+    if n == 0:
+    	return 1
+    if n == 1:
+    	return n 
+    return (1/factorial(n) + e_cuadratica(n-1))
 
 
 def e_lineal(n):
     # Implemente esta función
-    return 0
+    for i in n:
+   		a = sum(1/factorial(i))
+    return a
